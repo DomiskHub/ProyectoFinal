d8d3fcea73063a600aeab16b719671f8bb65a4ca
sebastian ignacio <sebastianalvarez6132@gmail.com>
2023-12-14 12:27:20 -0300

    componente signup

--- src/views/SignUp.jsx
+++ src/views/SignUp.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import React from 'react';
+import { Container, Form, Button, Card } from 'react-bootstrap';
 
 const SignUp = () => {
-  return <div> crear cuenta</div>;
+  return (
+    <Container className='signup-container'>
+      <Card className="signup-card mt-5">
+        <Card.Body>
+          <div className="signup-header">
+            <div className="texto">CREAR MI CUENTA</div>
+          </div>
+          <Form>
+            <Form.Group controlId="formFirstName" className="signup-input">
+            
+              <Form.Control type="text" placeholder="Nombre *" />
+            </Form.Group>
+
+            <Form.Group controlId="formLastName" className="signup-input">
+            
+              <Form.Control type="text" placeholder="Apellidos *" />
+            </Form.Group>
+
+            <Form.Group controlId="formEmail" className="signup-input">
+            
+              <Form.Control type="email" placeholder="Correo electronico *" />
+            </Form.Group>
+
+            <Form.Group controlId="formAddress" className="signup-input">
+            
+              <Form.Control type="text" placeholder="Direccion *" />
+            </Form.Group>
+
+            <div className="container-btn">
+              <Button className="signup-submit-btn" type="submit">
+                Crear mi cuenta
+              </Button>
+            </div>
+          </Form>
+        </Card.Body>
+      </Card>
+    </Container>
+  );
 };
 
 export default SignUp;
