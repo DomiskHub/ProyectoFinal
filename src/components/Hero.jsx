4be796b65576e205837afab20911076166d659ef
Daniela Leon <aleinadnoel@gmail.com>
2023-12-16 23:10:35 -0300

    cambios en el header y hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,42 +1,47 @@
 import { Carousel } from "react-bootstrap";
+import imgHero1 from "../assets/imgs/hero1.jpg"
+import imgHero2 from "../assets/imgs/somos-gif.gif"
+import imgHero3 from "../assets/imgs/hero3.jpg"
+import { Link } from "react-router-dom";
 
 const Hero = () => {
   return (
     <div>
       <Carousel className="carousel-hero">
         <Carousel.Item>
-          <img
-            className="d-block w-100 header-img"
-            src="https://www.shutterstock.com/image-photo/funny-ginger-cat-sitting-cardboard-600nw-1939653673.jpg"
-            alt="First slide"
-          />
+          <img className="d-block w-100 header-img" src={imgHero1} alt="Adopta un gato"/>
           <Carousel.Caption className="caption-hero">
             <h3>Adopta, NO COMPRES!</h3>
             <p>Hay gatitos esperando por ti</p>
+            <Link to="/galeria" className="m-1">
+                <button className= "btn nav-adopt-button">Ver Gatitos</button>
+            </Link>
           </Carousel.Caption>
         </Carousel.Item>
+
         <Carousel.Item>
-          <img
-            className="d-block w-100 header-img"
-            src="https://www.shutterstock.com/image-photo/funny-ginger-cat-sitting-cardboard-600nw-1939653673.jpg"
-            alt="Second slide"
-          />
+          <img className="d-block w-100 header-img" src={imgHero2} alt="Adopta un gato"/>
           <Carousel.Caption className="caption-hero">
-            <h3>Título de la diapositiva 2</h3>
-            <p>Descripción de la diapositiva 2.</p>
+            <h3>¿Quieres saber más sobre nosotros?</h3>
+            <p>Somos un grupo de Karens</p>
+            <Link to="/quienes_somos" className="m-1">
+                <button className= "btn nav-adopt-button">Quiénes Somos</button>
+            </Link>
           </Carousel.Caption>
         </Carousel.Item>
+
         <Carousel.Item>
-          <img
-            className="d-block w-100 header-img"
-            src="https://www.shutterstock.com/image-photo/funny-ginger-cat-sitting-cardboard-600nw-1939653673.jpg"
-            alt="Second slide"
-          />
+          <img className="d-block w-100 header-img" src={imgHero3} alt="Adopta un gato"/>
           <Carousel.Caption className="caption-hero">
-            <h3>Título de la diapositiva 3</h3>
-            <p>Descripción de la diapositiva 3.</p>
+            <h3>¿Tienes dudas?</h3>
+            <p>Escríbenos o visitanos</p>
+            <Link to="/#" className="m-1">
+                <button className= "btn nav-adopt-button">Contáctanos</button>
+            </Link>
           </Carousel.Caption>
         </Carousel.Item>
+
+
       </Carousel>
     </div>
   );
