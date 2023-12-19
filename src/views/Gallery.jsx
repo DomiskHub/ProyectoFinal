fb21b8c2476c6aecd11225b22f61eb09c1d76ccb
Dominque Piffardi <domiskp@gmail.com>
2023-12-19 19:37:45 -0300

    search added

--- src/views/Gallery.jsx
+++ src/views/Gallery.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { GlobalContext } from "../context/CardContext.jsx";
 import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
@@ -9,50 +9,55 @@ import { Link } from "react-router-dom";
 import IconHeart from "../components/IconHeart.jsx";
 
 const Gallery = () => {
+  const [search, setSearch] = useState("");
   const { cats, toggleFavoritePhoto } = useContext(GlobalContext);
   const navigate = useNavigate();
+
+  const filteredCats = cats.filter((cat) => cat.nombre.toLowerCase().includes(search.toLowerCase()));
 
   return (
     <Container>
       <div>
         <h1 className="m-4 text-center">Gatitos en adopción</h1>
+        <div className="input-search">
+          <input type="text" placeholder="Buscar por nombre..." value={search} onChange={(e) => setSearch(e.target.value)} className="m-2 p-2 input-style" />
+        </div>
       </div>
       <div className="grid-container">
-        {cats &&
-          cats.map((cat, index) => (
-            <Card className="text-center" key={index} style={{ width: "18rem" }}>
-              <Card.Img className="catcard-img" variant="top" src={cat.imagen} />
-              <div className="icon" onClick={() => toggleFavoritePhoto(cat)}>
-                <IconHeart filled={cat.liked} />
-              </div>
-              <Card.Body>
-                <Card.Title>{cat.nombre}</Card.Title>
-                <Card.Text></Card.Text>
-              </Card.Body>
-              <ListGroup className="list-group-flush">
-                <ListGroup.Item>
-                  <strong>Sexo:</strong> {cat.sexo}
-                </ListGroup.Item>
-                <ListGroup.Item>
-                  <strong>Color:</strong> {cat.color}
-                </ListGroup.Item>
-                <ListGroup.Item>
-                  <strong>Edad:</strong> {cat.edad}
-                </ListGroup.Item>
-                <ListGroup.Item>
-                  <div className="d-flex justify-content-center">
-                    <Link to="/adopta">
-                      <Button className="button-card button-card-gallery">Adoptar</Button>
-                    </Link>
-                    {/* //al boton mas info le hice la funcion directamente (funcion anonima) */}
-                    <Button className="button-card" onClick={() => navigate(`/detalle-gato/${cat.id}`)}>
-                      Mas info
-                    </Button>
-                  </div>
-                </ListGroup.Item>
-              </ListGroup>
-            </Card>
-          ))}
+        {filteredCats.map((cat, index) => (
+          <Card className="text-center" key={index} style={{ width: "18rem" }}>
+            <Card.Img className="catcard-img" variant="top" src={cat.imagen} />
+            <div className="icon" onClick={() => toggleFavoritePhoto(cat)}>
+              <IconHeart filled={cat.liked} />
+            </div>
+            <Card.Body>
+              <Card.Title>{cat.nombre}</Card.Title>
+              <Card.Text></Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+              <ListGroup.Item>
+                <strong>Sexo:</strong> {cat.sexo}
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <strong>Color:</strong> {cat.color}
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <strong>Edad:</strong> {cat.edad}
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <div className="d-flex justify-content-center">
+                  <Link to="/adopta">
+                    <Button className="button-card button-card-gallery">Adoptar</Button>
+                  </Link>
+                  {/* //al boton mas info le hice la funcion directamente (funcion anonima) */}
+                  <Button className="button-card" onClick={() => navigate(`/detalle-gato/${cat.id}`)}>
+                    Mas info
+                  </Button>
+                </div>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        ))}
       </div>
     </Container>
   );
