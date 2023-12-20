d56e96156d4b0b51ef56406539c721f2b966472e
Dominque Piffardi <domiskp@gmail.com>
2023-12-19 23:00:54 -0300

    login added

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,9 @@ import AdoptionForm from "./views/AdoptionForm";
 import Notfound from "./views/NotFound";
 import SignUp from "./views/SignUp";
 import Login from "./views/Login";
-import Contact from "./views/Contact"
+import Contact from "./views/Contact";
 import Profile from "./views/Profile";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
@@ -24,12 +25,19 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/quienes_somos" element={<AboutUs />} />
             <Route path="/galeria" element={<Gallery />} />
-            <Route path='/detalle-gato/:id' element={ <DetailCard /> } />
+            <Route path="/detalle-gato/:id" element={<DetailCard />} />
             <Route path="/adopta" element={<AdoptionForm />} />
             <Route path="/iniciar_sesion" element={<Login />} />
             <Route path="/crear_cuenta" element={<SignUp />} />
             <Route path="/contacto" element={<Contact />} />
-            <Route path="/perfil" element={<Profile/>} />
+            <Route
+              path="/perfil"
+              element={
+                <ProtectedRoute>
+                  <Profile />
+                </ProtectedRoute>
+              }
+            />
             <Route path="*" element={<Notfound />} />
           </Routes>
           <Footer />
